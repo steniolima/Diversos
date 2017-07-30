ee6bacbb0b8dc0842b953f556b5820442e9f90c6
steniolima <steniolimabarreto@gmail.com>
2017-07-30 02:42:19 -0300

    Codigos diversos - Construtores na Linguagem Java

---
+++ Teste.java
@@ -0,0 +1,34 @@
+package testeubuntu;
+
+public class Teste {
+	
+	private String cor;
+	private double preco;
+	private String modelo;
+	
+	
+	
+	public Teste () {			
+		
+	}
+	
+	
+	public Teste (String modelo, double preco) {
+		
+		this.cor = "PRETA";
+		this.modelo = modelo;
+		this.preco = preco;
+			
+	}
+	
+	
+	public Teste (String nome, double preco, String modelo) {
+		
+		this.cor = cor;
+		this.modelo = modelo;
+		this.preco = preco;
+		
+	}
+	
+}
+	
---
+++ TesteMain.java
@@ -0,0 +1,25 @@
+package testeubuntu;
+
+import javax.swing.JFrame;
+
+public class TesteMain {
+	
+	public static void main (String[]args) {
+	
+	   Teste testeprimeiro = new Teste();
+	
+       Teste testePRETO = new Teste ("Golf", 38000); 
+    
+       Teste testeAMARELO = new Teste ("Fox", 45000);
+
+	}
+	
+	
+	public void Print () {
+		
+		System.out.println("");
+		
+		
+	}
+	
+}
